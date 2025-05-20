e3aa6150eeb8ce43e4fba4063034a11f91864358
donchris90 <donchris4life2006@gmail.com>
2025-05-20 12:21:26 +0000

    changed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from database import  load_jobs_from_db
+from database import *
 
 
 app = Flask(__name__)
@@ -9,6 +9,13 @@ app = Flask(__name__)
 def home():
   jobs = load_jobs_from_db()
   return  render_template("home.html", jobs=jobs,company_name='Christo')
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  return render_template("jobpage.html", job=job)
+  
+ 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,18 +1,23 @@
-from sqlalchemy import create_engine,text
+# database.py
+from sqlalchemy import create_engine, text
 import os
 
+# Replace with your actual connection string
 db_connection_string = os.environ['DB_CONNECTION_STRING']
-engine = create_engine(db_connection_string,connect_args={
-                       "ssl": {
-                          "ca": "isrgrootx1.pem",
-                           
-                       }}
-                      )
-
-def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    jobs = []
-    for row in result.all():
-        jobs.append(row)
-    return jobs
+engine = create_engine(db_connection_string, connect_args={
+"ssl": {
+    "ca": "isrgrootx1.pem",
+}}
+)
+def load_job_from_db(id):
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(
+            text("SELECT * FROM jobs WHERE id = :val"),
+            {"val": id}
+    )
+            row = result.mappings().fetchone()
+            return dict(row) if row else None
+    except Exception as e:
+        print(f"Error occurred while loading job with id {id}: {e}")
+        return None
